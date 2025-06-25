2466e2c93700e9754823044590618d90427642bc
GOKULAKRISHNAN57 <149427308+GOKULAKRISHNAN57@users.noreply.github.com>
2025-06-25 22:38:22 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (91.70%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int st = 0;
+        boolean check = false;
+
+        for(int i = 0; i < m; i++){
+            int n = matrix[i].length;
+            int end = matrix[i].length;
+
+            for(int j = 0; j < n; j++){
+                int mid = (st+end)/2;
+
+                if(matrix[i][mid] == target){
+                    check = true;
+                    break;
+                }
+                if(matrix[i][mid] > target){
+                    end = mid;
+                }
+                else{
+                    st = mid;
+                }
+            }
+
+            st = 0;
+
+        }
+        return check;
+    }
+}
