66d5f18035053c8f776f2f0e6a4522974e48e520
M. Gokulakrishnan <149427308+GOKULAKRISHNAN57@users.noreply.github.com>
2025-08-09 22:05:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (6.56%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,9 +1,7 @@
 class Solution {
     public int mySqrt(int x) {
-
-        double squareroot = Math.sqrt(x);
-
-        int round = (int) Math.floor(squareroot);
-        return round;   
+        double newx = x;
+        newx =  Math.sqrt(newx);
+        return (int) newx;
     }
 }
