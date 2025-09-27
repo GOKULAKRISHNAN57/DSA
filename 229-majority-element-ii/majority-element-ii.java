b5984c4bc4eeee68f0c7431a3c18f792bffec64e
M. Gokulakrishnan <149427308+GOKULAKRISHNAN57@users.noreply.github.com>
2025-09-27 16:28:06 +0530

    Time: 20 ms (8.57%) | Memory: 49.3 MB (95.70%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,39 +1,27 @@
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.ArrayList;
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
 
-        ArrayList<Integer> newarray = new ArrayList<>();
-        
-        if(nums.length == 0 || nums.length == 1 || (nums.length == 2 && nums[0] != nums[1]) ){
-            for(int num : nums){
-                newarray.add(num);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(!(map.containsKey(nums[i]))){
+                map.put(nums[i],1);
             }
-            return newarray;
-        }
+            else{
+                map.put(nums[i],map.get(nums[i])+1);
+            }
 
-        int determine = nums.length/3;
-
-        int max = 1;
-        Arrays.sort(nums);
-
-        for(int j = 1 ; j < nums.length ; j++){
-            int i = j-1;
-            
-            if(nums[i] == nums[j]){
-                max++;
-
-                if(j+1 == nums.length || nums[j] != nums[j+1]){
-                    if(max > determine){
-                        newarray.add(nums[j]);
-                    }
-                    max = 1;
-                }
-
+            if( ( map.get(nums[i]) ) > (nums.length)/3 && !(list.contains(nums[i]))){
+                list.add(nums[i]);
             }
 
         }
-        return newarray;
+
+        return list;
     }
 }
