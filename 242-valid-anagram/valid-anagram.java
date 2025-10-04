8021741174deaf4f6e47eae80185b2bfe428b119
M. Gokulakrishnan <149427308+GOKULAKRISHNAN57@users.noreply.github.com>
2025-10-04 20:54:46 +0530

    Time: 4 ms (76.18%) | Memory: 44.9 MB (25.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -14,15 +14,6 @@ class Solution {
         Arrays.sort(c1);
         Arrays.sort(c2);
 
-        String s1 = new String(c1);
-        String t1 = new String(c2);
-
-        for(int i = 0; i < s1.length(); i++){
-            if(s1.charAt(i) != t1.charAt(i)){
-                return false;
-            }
-        }
-
-        return true;        
+        return Arrays.equals(c1,c2);     
     }
 }
